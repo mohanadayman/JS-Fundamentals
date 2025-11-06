004436a09d31712ae556ac63ef7c9510ce28f623
Mohanad <mohanad5ayman@gmail.com>
2025-11-06 15:52:53 +0200

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,8 @@
 const { argv } = require('node:process');
 
-if (argv.length === 0) {
+if (argv.length == 0) {
   console.log('No argument');
-} else if (argv.length === 1) {
+} else if (argv.length == 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
