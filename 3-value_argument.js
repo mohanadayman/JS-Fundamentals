e8e6772140321d8a3a0e789400a83f641f9c1304
Mohanad <mohanad5ayman@gmail.com>
2025-11-06 16:06:34 +0200

    Update 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,7 +1,5 @@
 const argv = process.argv.slice(2);
 
-if (argv.length === 0) {
-  console.log('No argument');
-} else {
-  console.log(argv[0]);
-}
+const first = process.argv[2];
+
+console.log(first ?? 'No argument');
